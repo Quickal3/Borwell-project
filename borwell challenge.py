d127ebf7de91eddae0ad8a418d8f036a32336f12
Abdul Albak <Abdulrahmanali4657@nwhsmail.co.uk>
2023-02-23 19:49:40 +0000

    added the formula to work out the surface area

--- borwell challenge.py
+++ borwell challenge.py
@@ -2,3 +2,4 @@ print("welcome to this program to use for a paint calculator")
 height = int(input("what is the height of the room?"))
 length = int(input("what is the length of the room?"))
 width = int(input("what is the width of the room?"))
+surface area = width*height*2 + length*width*2 + height*length*2
