2c9f2641205b8ec3be1b71926ada2a551febc179
Abdul Albak <Abdulrahmanali4657@nwhsmail.co.uk>
2023-02-23 19:40:09 +0000

    first commit

---
+++ borwell challenge.py
@@ -0,0 +1,4 @@
+print("welcome to this program to use for a paint calculator")
+height = int(input("what is the height of the room?"))
+length = int(input("what is the length of the room?"))
+width = int(input("what is the width of the room?"))
